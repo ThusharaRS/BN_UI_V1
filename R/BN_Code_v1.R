23f3945c0c74eeb5872df37e5d5bdbde0becb9b4
thusharars <44188247+ThusharaRS@users.noreply.github.com>
2019-04-24 12:49:42 +0530

    Update BN_Code_v1.R

--- R/BN_Code_v1.R
+++ R/BN_Code_v1.R
@@ -10,7 +10,7 @@
   lapply(Packages, library, character.only = TRUE)
 displayJson <- function(variableSelected,blList, wlList) {
   
-  variableSelected<-"Regular SSD while eating dinner away,KO with friend,Imagery - KO is unique and different from other soft drinks,KO while watching tv at home,Regular SSD alone or by myself,Age Nets,Any Bev while at work,Any Bev to celebrate with others"
+variableSelected<-"Regular SSD while eating dinner away,KO with friend,Imagery - KO is unique and different from other soft drinks,KO while watching tv at home,Regular SSD alone or by myself,Age Nets,Any Bev while at work,Any Bev to celebrate with others"
 wlList<-"Regular SSD while eating dinner away,KO with friend,KO with friend,Imagery - KO is unique and different from other soft drinks"
 blList<-"KO while watching tv at home,Age Nets,Regular SSD alone or by myself,Age Nets,Any Bev while at work,Any Bev to celebrate with others"
 
@@ -20,48 +20,45 @@ blList<-"KO while watching tv at home,Age Nets,Regular SSD alone or by myself,Ag
   myVector_WL<-unlist(strsplit(wlList,","))
   myVector_BL<-unlist(strsplit(blList,","))
 
-return myVector_WL
-  #Data_0 <- filter(Data, Data$KO_Consumption_4_5 != 99)
-  #names(Data_0) = Mapping$New_Variable[match(names(Data_0), Mapping$Old_Variable)]
+  Data_0 <- filter(Data, Data$KO_Consumption_4_5 != 99)
+  names(Data_0) = Mapping$New_Variable[match(names(Data_0), Mapping$Old_Variable)]
 
-  #Data_1 <- sapply(Data_0,as.factor)
- # Data_2 <- as.data.frame(Data_1)
+  Data_1 <- sapply(Data_0,as.factor)
+  Data_2 <- as.data.frame(Data_1)
 
- # myVector_var<-unlist(strsplit(variableSelected,","))
- # Data_3 <- Data_2[, c(myVector_var)]
+  myVector_var<-unlist(strsplit(variableSelected,","))
+  Data_3 <- Data_2[, c(myVector_var)]
   
   ## Black Listing
- # BL <- matrix(c(myVector_BL),
+  BL <- matrix(c(myVector_BL),
                ncol = 2, byrow = TRUE)
 
   # White Listing
- # WL <- matrix(c(myVector_WL),
- #              ncol = 2, byrow = TRUE)
+  WL <- matrix(c(myVector_WL),
+               ncol = 2, byrow = TRUE)
 
   #### score-based structure learning algorithms - HILL CLIMBING
-  #BN_HC <- hc(Data_3, score = "aic", whitelist = WL, blacklist = BL)
-  #BN_HC
+  BN_HC <- hc(Data_3, score = "aic", whitelist = WL, blacklist = BL)
 
-  #Score_BN <- score(BN_HC,Data_3)
-  #Score_BN
+  Score_BN <- score(BN_HC,Data_3)
 
-  #acyclic(BN_HC, directed = FALSE, debug = FALSE)
-  #directed(BN_HC)
-  #Arcs_DF <- as.data.frame(arcs(BN_HC))
-  #Arcs_DF$Unique <- paste(Arcs_DF$from,Arcs_DF$to,sep = "-")
+  acyclic(BN_HC, directed = FALSE, debug = FALSE)
+  directed(BN_HC)
+  Arcs_DF <- as.data.frame(arcs(BN_HC))
+  Arcs_DF$Unique <- paste(Arcs_DF$from,Arcs_DF$to,sep = "-")
 
-  #Boot_Strength <- boot.strength(Data_3, cluster = NULL, R = 100, m = nrow(Data_3), algorithm = "hc", algorithm.args = list(), cpdag = TRUE, debug = FALSE)
+  Boot_Strength <- boot.strength(Data_3, cluster = NULL, R = 100, m = nrow(Data_3), algorithm = "hc", algorithm.args = list(), cpdag = TRUE, debug = FALSE)
 
-  #Boot_Strength_DF <- data.frame(From = Boot_Strength$from, To = Boot_Strength$to,
-   #                              strength = Boot_Strength$strength,
-    #                             direction = Boot_Strength$direction
-  #)
+  Boot_Strength_DF <- data.frame(From = Boot_Strength$from, To = Boot_Strength$to,
+                                 strength = Boot_Strength$strength,
+                                 direction = Boot_Strength$direction
+  )
 
-  #Boot_Strength_DF$Unique <- paste(Boot_Strength_DF$From,Boot_Strength_DF$To,sep = "-")
-  #Arcs_BN <- merge(Arcs_DF,Boot_Strength_DF,by = "Unique")
-  #request.body <- toJSON(Arcs_BN[,-c(1:3,7)])
+  Boot_Strength_DF$Unique <- paste(Boot_Strength_DF$From,Boot_Strength_DF$To,sep = "-")
+  Arcs_BN <- merge(Arcs_DF,Boot_Strength_DF,by = "Unique")
+  request.body <- toJSON(Arcs_BN[,-c(1:3,7)])
 
- # return (request.body)
+  return (request.body)
   
 }
 #blList<-"KO during media consumption at leisure,KO Consumption,Any Bev to renew my energy,Income Level"
