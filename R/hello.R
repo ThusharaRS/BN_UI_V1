0b998469d4bdc6cccad34de98025833219dcf9ca
thusharars <44188247+ThusharaRS@users.noreply.github.com>
2019-04-25 15:07:01 +0530

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -27,9 +27,7 @@ blList<-"KO while watching tv at home,Age Nets,Regular SSD alone or by myself,Ag
    data(Mapping)
    
    Data_0 <- filter(Data, Data$KO_Consumption_4_5 != 99)
-    names(Data_0) = Mapping$New_Variable[match(names(Data_0), Mapping$Old_Variable)]
-   Data_1 <- sapply(Data_0,as.factor)
-  Data_2 <- as.data.frame(Data_1)
-   
-  return (Data_2)
+   names(Data_0) = Mapping$New_Variable[match(names(Data_0), Mapping$Old_Variable)]
+   new_var<-names(Data_0)
+  return (new_var)
 }
