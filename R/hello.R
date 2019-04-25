857ff287ef23b969ef5641a3a4e599b4abc28747
thusharars <44188247+ThusharaRS@users.noreply.github.com>
2019-04-25 15:07:33 +0530

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -27,7 +27,7 @@ blList<-"KO while watching tv at home,Age Nets,Regular SSD alone or by myself,Ag
    data(Mapping)
    
    Data_0 <- filter(Data, Data$KO_Consumption_4_5 != 99)
-   names(Data_0) = Mapping$New_Variable[match(names(Data_0), Mapping$Old_Variable)]
-   new_var<-names(Data_0)
-  return (new_var)
+   #names(Data_0) = Mapping$New_Variable[match(names(Data_0), Mapping$Old_Variable)]
+   #new_var<-names(Data_0)
+  return ( Data_0r)
 }
